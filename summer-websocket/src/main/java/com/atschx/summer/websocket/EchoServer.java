76cc51f8fad9ad1f650fb8b0b5fc1a3f986b9184
Albert Sun <atschx@gmail.com>
2015-04-28 01:41:16 +0800

    fix compile err

--- summer-websocket/src/main/java/com/atschx/summer/websocket/EchoServer.java
+++ summer-websocket/src/main/java/com/atschx/summer/websocket/EchoServer.java
@@ -1,13 +1,22 @@
 package com.atschx.summer.websocket;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.servlet.annotation.WebServlet;
 import javax.websocket.OnMessage;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
 
 
 @WebServlet
 @ServerEndpoint("/echo")
 public class EchoServer {
+	
+	Set<Session> persons =  Collections.synchronizedSet(new HashSet<Session>());
+	
 	@OnMessage
 	public String handleMessage(String message) {
 		return "Got your message (" + message + "). Thanks !";
