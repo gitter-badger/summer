3284f0dda428b2d05944cf54a432e514a072b3e1
Albert Sun <atschx@gmail.com>
2015-05-12 23:28:11 +0800

    add summer-boot

---
+++ summer-boot/src/main/java/com/atschx/summer/boot/SummerBootstrap.java
@@ -0,0 +1,20 @@
+package com.atschx.summer.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class SummerBootstrap {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SummerBootstrap.class, args);
+	}
+}
